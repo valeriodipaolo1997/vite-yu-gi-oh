dda47700bba0d8059110f5fc12e8c9531e90de72
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-09-26 16:18:02 +0200

    import axios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+      url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    }
+  },
+  created() {
+    for (let i = 0; i < 20; i++) {
+      axios
+        .get(this.url)
+        .then(response => {
+          console.log(response.data.data[i].name);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  }
+}
 
 </script>
 
-<template>
-  
-</template>
+<template></template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
